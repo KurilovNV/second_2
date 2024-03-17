e4abaa16a409f2cbd3515dfc4ff8605f022da6ff
Никита Курилов <nvkurilov@edu.hse.ru>
2024-03-17 23:30:44 +0500

    New

--- SecondTest/UnitTest1.cs
+++ SecondTest/UnitTest1.cs
@@ -20,40 +20,55 @@ namespace SecondTest
             public async Task GetTasksFromServer_ReturnsTasks()
             {
                 // Arrange
-                var expectedTasks = new List<TaskModel>
-            {
-                new TaskModel { Question = "Question1", Answer = "Answer1" },
-                new TaskModel { Question = "Question2", Answer = "Answer2" }
-            };
-
-                var mockHttpMessageHandler = new Mock<HttpMessageHandler>(MockBehavior.Strict);
+                var mockHttpMessageHandler = new Mock<HttpMessageHandler>();
                 mockHttpMessageHandler
                     .Protected()
                     .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
                     .ReturnsAsync(new HttpResponseMessage
                     {
                         StatusCode = HttpStatusCode.OK,
-                        Content = new StringContent("[{\"Question\":\"Question1\",\"Answer\":\"Answer1\"},{\"Question\":\"Question2\",\"Answer\":\"Answer2\"}]"),
+                        Content = new StringContent("[{\"Question\":\"Question1\",\"Answer\":\"Answer1\"},{\"Question\":\"Question2\",\"Answer\":\"Answer2\"}]")
                     });
 
                 var httpClient = new HttpClient(mockHttpMessageHandler.Object);
                 var controller = new RemoteTaskController(httpClient);
 
                 // Act
-                var result = await controller.GetTasksFromServer() as OkObjectResult;
+                var result = await controller.GetTasksFromServer();
 
                 // Assert
                 Assert.NotNull(result);
-                Assert.AreEqual((int)HttpStatusCode.OK, result.StatusCode);
+                Assert.IsInstanceOf<Microsoft.AspNetCore.Mvc.OkObjectResult>(result);
 
-                var tasks = result.Value as List<TaskModel>;
-                Assert.NotNull(tasks);
-                Assert.AreEqual(expectedTasks.Count, tasks.Count);
-                for (int i = 0; i < expectedTasks.Count; i++)
-                {
-                    Assert.AreEqual(expectedTasks[i].Question, tasks[i].Question);
-                    Assert.AreEqual(expectedTasks[i].Answer, tasks[i].Answer);
-                }
+                var okResult = (Microsoft.AspNetCore.Mvc.OkObjectResult)result;
+                Assert.AreEqual(200, okResult.StatusCode);
+
+                var content = okResult.Value.ToString();
+                Assert.AreEqual("[{\"Question\":\"Question1\",\"Answer\":\"Answer1\"},{\"Question\":\"Question2\",\"Answer\":\"Answer2\"}]", content);
+            }
+            [Test]
+            public async Task GetTasksFromServer_ReturnsStatusCodeResult_WhenHttpClientReturnsError()
+            {
+                // Arrange
+                var mockHttpMessageHandler = new Mock<HttpMessageHandler>();
+                mockHttpMessageHandler
+                    .Protected()
+                    .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
+                    .ReturnsAsync(new HttpResponseMessage
+                    {
+                        StatusCode = HttpStatusCode.InternalServerError
+                    });
+
+                var httpClient = new HttpClient(mockHttpMessageHandler.Object);
+                var controller = new RemoteTaskController(httpClient);
+
+                // Act
+                var result = await controller.GetTasksFromServer();
+
+                // Assert
+                Assert.IsInstanceOf<ObjectResult>(result);
+                var statusCodeResult = result as ObjectResult;
+                Assert.AreEqual((int)HttpStatusCode.InternalServerError, statusCodeResult.StatusCode);
             }
         }
 
